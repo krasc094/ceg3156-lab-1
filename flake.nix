a6648753b5afb98c4f4a0b76ed2c4c1d9b3a40f9
Kai R <stfourrtup@gmail.com>
2026-01-19 15:07:54 -0500

    first commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            vhdl-ls # language server
+
+            ghdl # compile / simulate vhdl
+            gtkwave # waveform visualizer
+
+            netlistsvg
+            yosys
+            yosys-ghdl
+
+            mermaid-cli
+
+            gnumake # make
+          ];
+        };
+    });
+}
